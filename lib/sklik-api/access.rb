932e978f06f60c13df481ca6373cb4cca8febda0
Ondřej Bartas <ondrej@bartas.cz>
2013-10-03 00:08:04 +0200

    [FIX] Access - use store method for accessing Thread.current[:sklik_api]

--- lib/sklik-api/access.rb
+++ lib/sklik-api/access.rb
@@ -1,6 +1,10 @@
 # -*- encoding : utf-8 -*-
 class SklikApi::Access
 
+  def self.store
+    Thread.current[:sklik_api] ||= {}
+  end
+
   #set credentials
   def self.set args = {}
     args.symbolize_keys!
@@ -9,64 +13,64 @@ class SklikApi::Access
     raise ArgumentError, "password is required" unless args[:password]
     
     #save argument to right places
-    Thread.current[:sklik_api] ||= {}
-    Thread.current[:sklik_api][:args] = args
+    
+    store[:args] = args
 
     #save session if present
-    Thread.current[:sklik_api][:session] = args.has_key?(:session) ? args[:session] : nil
+    store[:session] = args.has_key?(:session) ? args[:session] : nil
 
     #return this object!
     return self
   end
 
   def self.get
-    Marshal.load( Marshal.dump ( Thread.current[:sklik_api][:args] )) 
+    Marshal.load( Marshal.dump ( store[:args] )) 
   end
   
   #return email
   def self.email
-    Thread.current[:sklik_api][:args][:email].to_s
+    store[:args][:email].to_s
   end
 
   #for login take first part of email "name@seznam.cz" -> "name"
   def self.login
-    Thread.current[:sklik_api][:args][:email].to_s.split("@").first
+    store[:args][:email].to_s.split("@").first
   end
 
   #return customer_id
   def self.customer_id
-    Thread.current[:sklik_api][:args].has_key?(:customer_id) && Thread.current[:sklik_api][:args][:customer_id] ? Thread.current[:sklik_api][:args][:customer_id] : nil
+    store[:args].has_key?(:customer_id) && store[:args][:customer_id] ? store[:args][:customer_id] : nil
   end
 
   #return password
   def self.password
-    Thread.current[:sklik_api][:args][:password].to_s
+    store[:args][:password].to_s
   end
 
   #Set session
   def self.session= session
-    Thread.current[:sklik_api][:session] = session
+    store[:session] = session
   end
 
   #Get session
   def self.session
-    Thread.current[:sklik_api][:session]
+    store[:session]
   end
 
   #Has setted session
   def self.session?
-    !Thread.current[:sklik_api][:session].nil?
+    !store[:session].nil?
   end
     
   #if you change Access credentials change uniq identifier -> 
   #used for stroing sessions for multiple logins
   def self.uniq_identifier 
-    "#{Thread.current[:sklik_api][:args][:email]}:#{Thread.current[:sklik_api][:args][:password]}"
+    "#{store[:args][:email]}:#{store[:args][:password]}"
   end
   
   #to prevent changes in settings dump it
   def self.access
-    Marshal.load( Marshal.dump( Thread.current[:sklik_api][:args] ))
+    Marshal.load( Marshal.dump( store[:args] ))
   end
   
 end
