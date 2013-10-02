2fbe0824b6002d80f398aabb24d62db93599718c
Ondřej Bartas <ondrej@bartas.cz>
2013-10-03 00:56:23 +0200

    [FIX] Store XML RPC connection in class method + when IO Error then reopen connection and retry

--- lib/sklik-api/connection.rb
+++ lib/sklik-api/connection.rb
@@ -8,16 +8,9 @@ class SklikApi
       :timeout => 100
     }
 
-    def initialize args = {}
-      @args = DEFAULTS.merge(args)
-    end
-
-    def self.connection
-      @connection ||= SklikApi::Connection.new(:debug => false)
-    end
-
     #prepare connection to sklik
-    def connection
+    def self.connection
+      Thread.current[:sklik_api] ||= {}
       unless Thread.current[:sklik_api][:connection]
         path = (SklikApi.use_sandbox_for_test? && (ENV['RACK_ENV'] || ENV['RAILS'] == "test")) ? "/sandbox/RPC2" : "/RPC2"
         server = XMLRPC::Client.new3(:host => "api.sklik.cz", :path => path, :port => 443, :use_ssl => true, :timeout => @args[:timeout])
@@ -29,6 +22,30 @@ class SklikApi
         Thread.current[:sklik_api][:connection] = server
       end
       Thread.current[:sklik_api][:connection]
+    end
+
+    def reopen_connection!
+      Thread.current[:sklik_api] ||= {}
+      Thread.current[:sklik_api][:connection] = nil
+      connection
+    end
+
+    def initialize args = {}
+      @args = DEFAULTS.merge(args)
+    end
+
+    def self.connection
+      @connection ||= SklikApi::Connection.new(:debug => false)
+    end
+
+    #prepare connection to sklik
+    def connection
+      self.class.connection
+    end
+
+    #prepare connection to sklik
+    def reopen_connection!
+      self.class.reopen_connection!
     end
 
     def set_new_session response
@@ -77,6 +94,12 @@ class SklikApi
         rescue XMLRPC::FaultException => e
           raise ArgumentError, "#{e.faultString}, #{e.faultCode}"
         rescue Exception => e
+          #when closed IO -> retry
+          if e.class.name =~ /IOError/
+            reopen_connection!
+            retry
+          end
+
           raise e
         end
       end
@@ -112,6 +135,12 @@ class SklikApi
           raise ArgumentError, "There is error from sklik #{method}: #{param.inspect}: #{args.inspect}"
         end
       rescue Exception => e
+
+        #when closed IO -> retry
+        if e.class.name =~ /IOError/
+          reopen_connection!
+          retry
+        end
         #when know exception which is not fault of sklik don't retry!
         raise e if e.class.name =~ /SklikApi::/
 
